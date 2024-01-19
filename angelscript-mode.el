c1dfd0ffdd73a9a0b6fd9cff98ed1344712e6639
Max Düring <max.during@netlight.com>
2024-01-19 09:52:12 +0100

    Add private keyword

--- angelscript-mode.el
+++ angelscript-mode.el
@@ -6,7 +6,7 @@
     ("\\<\\(bool\\|void\\|int\\|float\\|char\\)\\>" . font-lock-builtin-face)
     ("\\<\\(true\\|false\\|nullptr\\)\\>" . font-lock-builtin-face)
 
-    ("\\<\\(default\\|delegate\\|const\\|struct\\|enum\\|if\\|auto\\|else\\|class\\|while\\|for\\|return\\|this\\)\\>" . font-lock-keyword-face)
+    ("\\<\\(private\\|default\\|delegate\\|const\\|struct\\|enum\\|if\\|auto\\|else\\|class\\|while\\|for\\|return\\|this\\)\\>" . font-lock-keyword-face)
     ("\\<\\(switch\\|case\\|break\\)\\>" . font-lock-keyword-face)
 
     ("\\<\\(UCLASS\\|UFUNCTION\\|UPROPERTY\\)\\>" . font-lock-constant-face)
